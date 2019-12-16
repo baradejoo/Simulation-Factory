20bd00c4cc6d2ef450d0b6e2fd6cb2dfad8adc1d
Robert <r.bytnar1@gmail.com>
2019-12-16 18:34:53 +0100

    Stworzenie pliku źródłowego storage_types

---
+++ src/storage_types.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Robert on 16.12.2019.
+//
+
+#include <storage_types.hpp>
+#include "storage_types.hpp"
+
+
+PackageQue::PackageQueue(PackageQueueType) {
+    switch(PackageQueueType){
+        case FIFO:
+
+            break;
+
+        case LIFO:
+
+            break;
+    }
+}
+
