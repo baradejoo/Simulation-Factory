a68f2692e03abcaa412ddd207cba59779a31442f
Robert <r.bytnar1@gmail.com>
2019-12-16 18:36:49 +0100

    test

--- include/storage_types.hpp
+++ include/storage_types.hpp
@@ -9,4 +9,25 @@ enum class PackageQueueType{
     FIFO, LIFO
 };
 
+class PackageQue : public IPackageQueue {
+public:
+    PackageQueue(PackageQueueType);
+};
+
+class IPackageQueue : public IPackageStockpile {
+public:
+    Package pop()
+    PackageQueueType const get_queue_type()
+};
+
+class IPackageStockpile{
+public:
+    void push(Package&&);
+    bool empty();
+    size_type size();
+
+};
+
+
+
 #endif //LAB10_STORAGE_TYPES_HPP
