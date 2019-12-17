20c65eb156fb37077d9d70ac10d13073ad6a8e72
baradejoo <baradziejkamil@gmail.com>
2019-12-17 21:17:54 +0100

    Konfiguracja Cmake + dodanie GoogleTests

--- include/storage_types.hpp
+++ include/storage_types.hpp
@@ -1,13 +1,20 @@
-//    klasy IPackageStockpile, IPackageQueue, PackageQueue
-//    typ wyliczeniowy PackageQueueType
-
+// <<interface>> IPackageStockpile, <<interface>> IPackageQueue //
+// <<class>> PackageQueue //
+// <<enumeration>> PackageQueueType //
 
 #ifndef LAB10_STORAGE_TYPES_HPP
 #define LAB10_STORAGE_TYPES_HPP
 
+#include <deque>
+#include <iostream>
+#include "package.hpp"
+
 enum class PackageQueueType{
     FIFO, LIFO
 };
+
+class const_iterator;
+
 /*
 class PackageQue : public IPackageQueue {
 public:
@@ -22,9 +29,12 @@ public:
 
 class IPackageStockpile{
 public:
-    void push(Package&&);
-    bool empty();
-    size_type size();
+    using container_type=std::deque<Package>;
+    using size_type=unsigned int;
+
+    void push(Package&&) {container_type.push_back(&Package);}
+    bool empty() const {return container_type.empty()
+    size_type size() const {return container_type.size();}
 
 };
 
