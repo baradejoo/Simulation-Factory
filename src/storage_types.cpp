1d8685f91c93e2aec73b174970c6cd7fce929b7f
inpgbburda <bburdainpg@gmail.com>
2020-01-02 00:39:02 +0100

    zmiana sposobu zwracania dla PackageQueue

--- src/storage_types.cpp
+++ src/storage_types.cpp
@@ -5,18 +5,18 @@
 #include "storage_types.hpp"
 
 Package PackageQueue::pop() {
+    Package put_package;
     switch(queue_type){
         case PackageQueueType::FIFO: {
-            Package put_package = std::move(queue.front());
+            put_package = std::move(queue.front());
             queue.pop_front();
-            return put_package;
             break;
         }
         case PackageQueueType::LIFO: {
-            Package put_package = std::move(queue.back());
+            put_package = std::move(queue.back());
             queue.pop_back();
-            return put_package;
             break;
         }
     }
+    return put_package;
 }
