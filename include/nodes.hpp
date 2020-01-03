84a98a3012a47c6bb1390d37836c24928a9a0017
baradejoo <baradziejkamil@gmail.com>
2020-01-03 14:16:18 +0100

    finished Worker, almost finished Ramp, PackageSender (modify fabryka.cpp, doesnt work)

--- include/nodes.hpp
+++ include/nodes.hpp
@@ -18,6 +18,27 @@ enum class ReceiverType{
     Ramp, Worker, Storehouse
 };
 
+class ReceiverPreferences {
+public:
+    using preferences_I = std::map<IPackageReceiver*, double>;
+    using RecPreCIt = preferences_t::const_iterator;
+    using RecPreIt = preferences_t::iterator;
+
+    void add_reciever(IPackageReceiver* r);
+    void remove_reciever(IPackageReceiver* r);
+    IPackageReceiver* choose_receiver();
+
+    RecPreIt begin() = { return preferences_list_.begin(); };
+    const RecPreCIt cbegin() = { return preferences_list_.cbegin(); };
+    RecPreIt end() = { return preferences_list_.end(); };
+    const RecPreCIt cend() = { return preferences_list_.cend(); };
+
+
+private:
+    preferences_I preferences_list_;
+    Probal_gener rng_;
+};
+
 class IPackageReceiver{
 public:
     using pacReceiverIt = std::list<Package>::const_iterator;
@@ -34,41 +55,13 @@ public:
     virtual ~IPackageReceiver() {};
 };
 
-class Worker : public IPackageReceiver{
-public:
-    Worker(ElementID id, TimeOffset pd, std::unique_ptr<PackageQueue> q) : id_(id), pd_(pd), q_(std::move(q)) {}
-
-    Worker(Time t_);
-
-    Worker(const Worker&) = delete;
-    Worker& operator=(Worker&) = delete;
-
-    void do_work(Time t) { t_ = t; }
-    TimeOffset get_processing_duration() const {return pd_; };
-    Time get_package_processing_start_time() const { return t_; }
-
-    ReceiverType get_receiver_type() const override { return rec_tp; }
-    ElementID  get_id() const override { return id_; }
-    void receive_package(Package&& prod) override;
-
-    ~Worker() = default;
-
-private:
-    ElementID id_;
-    TimeOffset pd_;
-    Time t_{};
-    std::unique_ptr<PackageQueue> q_;
-    std::optional<Package> WorkerBuffer = std::nullopt;
-    ReceiverType rec_tp = ReceiverType::Worker;
-};
-
 class PackageSender {
 protected:
     void push_package(Package pack);
 
 public:
     void send_package();
-    std::optional<Package> get_sending_buffer();
+    std::optional<Package> get_sending_buffer() { return std::move(PackageSenderBuffor); }
     ReceiverPreferences receiver_preferences_;
 
 private:
@@ -76,11 +69,49 @@ private:
 
 };
 
+class Worker : public IPackageReceiver, public PackageSender{
+public:
+    Worker(ElementID id, TimeOffset pd, std::unique_ptr<PackageQueue> q) : id_(id), pd_(pd), q_(std::move(q)) {}
+
+    Worker(const Worker&) = delete;
+    Worker& operator=(Worker&) = delete;
+
+    void do_work(Time t);
+    TimeOffset get_processing_duration() const {return pd_; };
+    Time get_package_processing_start_time() const { return t_; }
+
+    ReceiverType get_receiver_type() const override { return rec_tp; }
+    ElementID  get_id() const override { return id_; }
+    void receive_package(Package&& prod) override;
+
+    pacReceiverIt begin() override { return q_->begin(); }
+    const pacReceiverIt cbegin() override { return q_->cbegin(); }
+    pacReceiverIt end() override { return q_->end(); }
+    const pacReceiverIt cend() override { return q_->cend(); }
+
+    ~Worker() = default;
+
+private:
+    ElementID id_;
+    TimeOffset pd_;
+    Time t_ = 0;
+    std::unique_ptr<PackageQueue> q_ = nullptr;
+    std::optional<Package> WorkerBuffer = std::nullopt;
+    ReceiverType rec_tp = ReceiverType::Worker;
+};
+
+
+//pacReceiverIt begin() override { return q_->begin(); }
+//const pacReceiverIt cbegin() override { return q_->cbegin(); }
+//pacReceiverIt end() override { return q_->end(); }
+//const pacReceiverIt cend() override { return q_->cend(); } //metody do Storehouse
+
+
 class Ramp : public PackageSender{
 public:
     Ramp(ElementID id, TimeOffset di) : id_(id), di_(di) {}
 
-    void deliver_goods(Time t) { t_ = t;};
+    void deliver_goods(Time t);
     TimeOffset get_delivery_interval() const { return di_; }
     ElementID  get_id() const { return id_; }
 
@@ -89,30 +120,7 @@ public:
 private:
     ElementID id_;
     TimeOffset di_;
-    Time t_{};
 };
 
-class ReceiverPreferences {
-public:
-    using preferences_t = std::map<IPackageReceiver*, double>;
-    using const_iterator = preferences_t::const_iterator;
-    using iterator = preferences_t::iterator;
 
-
-    void add_reciever(IPackageReceiver* r);
-
-    void remove_reciever(IPackageReceiver* r);
-
-    IPackageReceiver* choose_receiver();
-
-    const_iterator begin() = { return preferences_list.begin(); };
-    const const_iterator cbegin() = { return preferences_list.cbegin(); };
-    const_iterator end() = { return preferences_list.end(); };
-    const const_iterator cend() = { return preferences_list.cend(); };
-
-
-private:
-    IPackageReceiver* r;
-    preferences_t preferences_list;
-};
 #endif //FABRYKA_NODES_HPP
--- src/nodes.cpp
+++ src/nodes.cpp
@@ -6,24 +6,47 @@
 
 #include "nodes.hpp"
 
-void Worker::receive_package(Package&& prod) {
-    //
+void Worker::do_work(Time t) {
+    if((t-t_)%pd_ == 0){
+        if(WorkerBuffer) {
+            push_package(std::move(*WorkerBuffer));
+            WorkerBuffer.reset();
+        }
+        WorkerBuffer.emplace(q_->pop());
+        send_package();
+    }
+    t_=t;
 }
-
-
 
 void PackageSender::push_package(Package pack) {
     PackageSenderBuffor.emplace(std::move(pack));
 }
 
-void ReceiverPreferences::add_reciever(IPackageReceiver* r) {
-    //preferences_t.ins
+void Ramp::deliver_goods(Time t) {
+    if(t%di_ == 0){
+
+    }
 }
 
-void ReceiverPreferences::remove_reciever(IPackageReceiver* r) {
-   //preferences_t.erase()
+void Worker::receive_package(Package&& prod) {
+    q_->push(std::move(prod));
 }
 
-IPackageReceiver* ReceiverPreferences::choose_receiver() {
-    //for (preferences_t it = begin(); it != end(); it++)
-}
+
+//void PackageSender::send_package() {
+//    if(PackageSenderBuffor){
+//
+//    }
+//}
+
+//void ReceiverPreferences::add_reciever(IPackageReceiver* r) {
+//    //preferences_t.ins
+//}
+//
+//void ReceiverPreferences::remove_reciever(IPackageReceiver* r) {
+//   //preferences_t.erase()
+//}
+//
+//IPackageReceiver* ReceiverPreferences::choose_receiver() {
+//    //for (preferences_t it = begin(); it != end(); it++)
+//}
