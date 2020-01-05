62d173dfa33021b9ecd33c8d5b711e7faceb936c
xrobert9 <45435288+xrobert9@users.noreply.github.com>
2020-01-05 17:07:00 +0100

    Add files via upload

--- src/nodes.cpp
+++ src/nodes.cpp
@@ -29,6 +29,8 @@ void Ramp::deliver_goods(Time t) {
     }
 }
 
+Ramp::Ramp(ElementID id, TimeOffset di) : id_(id), di_(di) {}
+
 void Worker::receive_package(Package&& prod) {
     q_->push(std::move(prod));
 }
