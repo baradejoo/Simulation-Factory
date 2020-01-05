5596e3f6d7a4098c475d18047af190a2577610f8
xrobert9 <45435288+xrobert9@users.noreply.github.com>
2020-01-05 21:26:17 +0100

    Add files via upload

--- src/nodes.cpp
+++ src/nodes.cpp
@@ -14,24 +14,44 @@ void Worker::do_work(Time t) {
             WorkerBuffer.reset();
         }
         WorkerBuffer.emplace(q_->pop());
-        send_package();
+        //send_package();
+        t_=t;
     }
-    t_=t;
+
 }
 
-void PackageSender::push_package(Package pack) {
+void PackageSender::push_package(Package&& pack) {
     PackageSenderBuffor.emplace(std::move(pack));
 }
 
-void Ramp::deliver_goods(Time t) {
-    if(t%di_ == 0){
 
+void Ramp::deliver_goods(Time t)
+{
+    if(t % di_ == 0)
+    {
+        Package package;
+        push_package(std::move(package));
+        //send_package();
     }
 }
 
 void Worker::receive_package(Package&& prod) {
     q_->push(std::move(prod));
 }
+
+void Storehouse::receive_package(Package&& pck)
+{
+    d_->push(std::move(pck));
+
+}
+
+//Worker::Worker(ElementID id, TimeOffset pd, std::unique_ptr<IPackageQueue> q)
+//{
+//    id_ = id;
+//    pd_ = pd;
+//    q_ = std::move(q);
+//    preferences_list_ = preferences_list;
+//}
 
 void PackageSender::send_package()
 {
@@ -44,40 +64,51 @@ void PackageSender::send_package()
 
 
 void ReceiverPreferences::add_receiver(IPackageReceiver* r) {
-    //double sum_temp = 0.0;
-    double random_number = pg();
+    double sum_temp = 0.0;
 
-    if (preferences_list_.empty()){
-        preferences_list_.insert(std::make_pair(r,1));
-    }
-    preferences_list_.insert(std::make_pair(r, random_number));
-    double sum_temp = 1 + random_number;
-    for ( auto& [key, value]: preferences_list_){
-        value = value/sum_temp;
+    preferences_list_.emplace(std::make_pair(r, 1.0));
+
+    for(const auto& item: preferences_list_)
+    {
+        sum_temp+= item.second;
     }
 
+    for(auto& item: preferences_list_)
+    {
+        item.second /= sum_temp;
+    }
 
 }
 
 void ReceiverPreferences::remove_receiver(IPackageReceiver* r) {
-    double random_number = pg();
+    double sum_temp = 0.0;
 
     preferences_list_.erase(r);
 
-    double sum_temp = 1 + random_number;
-    for ( auto& [key, value]: preferences_list_){
-        value = value/sum_temp;
+    for(const auto& item: preferences_list_)
+    {
+        sum_temp+= item.second;
     }
+
+    for(auto& item: preferences_list_)
+    {
+        item.second /= sum_temp;
+    }
+
 }
 
-bool compare(std::pair <std::string, int> p, std::pair <std::string, int> q )
+
+IPackageReceiver* ReceiverPreferences::choose_receiver()
 {
-    return p.second < q.second;
-}
-
-IPackageReceiver* ReceiverPreferences::choose_receiver() {
-    std::pair<IPackageReceiver*, double> max = (*std::max_element(preferences_list_.begin(), preferences_list_.end(), compare));
-    return max.first;
+    double number = pg();
+    double sum = 0;
+    for (const auto &item: preferences_list_)
+    {
+        sum += item.second;
+        if(number <= sum)
+            return item.first;
+    }
+    return preferences_list_.end()->first;
 }
 
 
