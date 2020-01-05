906f1d791b17b76b672f34ef4dbfed27eacd2d0a
Robert <r.bytnar1@gmail.com>
2020-01-05 12:50:51 +0100

    ReceiverPreferences

--- src/nodes.cpp
+++ src/nodes.cpp
@@ -42,12 +42,17 @@ void Worker::receive_package(Package&& prod) {
 void ReceiverPreferences::add_receiver(IPackageReceiver* r) {
     double sum_temp = 0.0;
 
-    preferences_list_.insert(std::pair<IPackageReceiver*, double> (r,0))
+    preferences_list_.insert({r,0});
 
-    for( auto& [key, val] : preferences_list_ )
+//    for( auto& [key, val] : preferences_list_ )
+//    {
+//        key = pg()*(1.0-pg())
+//        sum_temp += key
+//    }
+    for(auto iterator = preferences_list_.begin(); iterator != preferences_list_.end(); iterator++)
     {
-        key = pg()*(1.0-pg())
-        sum_temp += key
+        iterator ->first = pg()*(1.0-pg())
+        sum_temp += iterator ->first
     }
 
     auto it = preferences_list_.end();
@@ -60,12 +65,17 @@ void ReceiverPreferences::remove_receiver(IPackageReceiver* r) {
     double sum_temp = 0.0;
     //auto it = preferences_list_.end();
 
-    preferences_list_.erase(std::pair<IPackageReceiver*, double> (r,0))
+    preferences_list_.erase(r);
 
-    for( auto& [key, val] : preferences_list_ )
+//    for( auto& [key, val] : preferences_list_ )
+//    {
+//        key = pg()*(1.0-pg())
+//        sum_temp += key
+//    }
+    for(auto iterator = preferences_list_.begin(); iterator != preferences_list_.end(); iterator++)
     {
-        key = pg()*(1.0-pg())
-        sum_temp += key
+        iterator ->first = pg()*(1.0-pg())
+        sum_temp += iterator ->first
     }
 
     //auto it = preferences_list_.end()-- ->second = 1.0-sum_temp;
@@ -75,8 +85,18 @@ void ReceiverPreferences::remove_receiver(IPackageReceiver* r) {
 
 }
 
-//IPackageReceiver* ReceiverPreferences::choose_receiver() {
+bool compare(std::pair <std::string, int> p, std::pair <std::string, int> q )
+{
+    return p.second < q.second;
+}
+
+IPackageReceiver* ReceiverPreferences::choose_receiver() {
 //    //for (preferences_t it = begin(); it != end(); it++)
-//}
+
+
+    std::pair<IPackageReceiver*, double> max = (*std::max_element(preferences_list_.begin(), preferences_list_.end(), compare));
+    return max.first;
+
+}
 
 
