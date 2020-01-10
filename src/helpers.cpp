05e35aab5851fcf85e9bbeb323a86d31913631c4
baradejoo <baradziejkamil@gmail.com>
2020-01-10 19:54:49 +0100

    Receiverpreferences - add_receiver done

--- src/helpers.cpp
+++ src/helpers.cpp
@@ -1,4 +1,13 @@
-//
-// Created by Robert on 05.01.2020.
-//
+//=============== default_probability_generator - definition ================//
 
+#include "helpers.hpp"
+
+std::random_device rd;
+std::mt19937 rng_(rd());
+
+double default_probability_generator(){
+    double rand_temp = std::generate_canonical<double, 10>(rng_);
+    return rand_temp;
+}
+
+std::function<double()> probability_generator = default_probability_generator;
